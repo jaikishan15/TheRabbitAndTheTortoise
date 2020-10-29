00ed1e9c0687c2f5912af5a3b94f1b2438223182
jaikishan15 <jai.kishan_cs19@gla.ac.in>
2020-10-29 12:46:09 +0530

    add statement for dialog box to show message

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -56,6 +56,12 @@ public class Tortoise extends Thread {
                 System.err.println("The tortoise has lost the path!");
             }
         }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
             }
         }
 

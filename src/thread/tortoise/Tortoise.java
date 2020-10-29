907a62f93e02a3c8f07a4e884d3ab0e3a679271d
jaikishan15 <jai.kishan_cs19@gla.ac.in>
2020-10-29 12:38:22 +0530

    add a private float type variable time taken by tortoise to complete the race

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -5,7 +5,11 @@ import javax.swing.*;
 public class Tortoise extends Thread {
     private static final int MILESTONES = 5;
 
-    }
+    private final Thread tortoise;
+    private float timeTakenByTortoise = 0f;
+
+
+}
 
 
 
